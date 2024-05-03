ea0d0d72d2a8195bb23f137d54b1c0ed28d84f8b
Yiwan <zhuyiwan@gmail.com>
2024-05-03 17:05:18 +0800

    Final cat

--- CRM/notes/models.py
+++ CRM/notes/models.py
@@ -9,6 +9,11 @@ class BasicModelTemplate(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
 
+COEFICIENT = {
+    "Блок": 3,
+    "К выполнению": 2,
+    "Коментарий": 1,
+}
 
 class ProfilesRoles(models.TextChoices):
     admin = "Администратор"
@@ -17,13 +22,32 @@ class ProfilesRoles(models.TextChoices):
     client = "Килент"
     operaton = "Оператор" 
 
+# def get_raiting(queryset):
+#         queryset = Notes.objects.all()
+#         raiting = [-1, 1]
+#         likes_topics = queryset.filter(category)
+#         likes_tasks
+#         likes_comments 
+
+
 class Profiles(BasicModelTemplate):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     department = models.CharField(max_length=100, blank=True, null=True)
     role = models.CharField(max_length=20, choices=ProfilesRoles.choices)
-    raiting = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
 
     def renew_raiting(self):
+        global COEFICIENT
+        sum = 0
+        queryset = NotesReaction.objects.all()
+        for key, value in COEFICIENT.items(): 
+            likes_topics = queryset.filter(note__created_by=self, reaction=True, note__category=key)
+            sum += likes_topics.count() * value
+        self.likes = sum
+
+        dislikes_topics = queryset.filter(note__created_by=self, reaction=True)
+        self.dislikes = dislikes_topics.count()
 
     def __str__(self):
         return f"{self.user.username} - {self.role}"
@@ -44,16 +68,36 @@ class Notes(BasicModelTemplate):
     title = models.CharField(max_length=100)
     content = models.TextField()
     notes_relations = models.ForeignKey('Notes', on_delete=models.CASCADE, related_name='note_parent', null=True, blank=True)
-    rating = models.IntegerField()
+    likes = models.IntegerField(default=0)
+    dislikes = models.IntegerField(default=0)
     category = models.CharField(max_length=16, choices=CategoryType.choices)
+
+    def renew_raiting(self):
+        queryset = NotesReaction.objects.all()
+        likes_topics = queryset.filter(note=self, reaction=True)
+        dislikes_topics = queryset.filter(note=self, reaction=False)
+        self.likes = likes_topics.count()
+        self.dislikes = dislikes_topics.count()
 
     def clean(self):
         if self.notes_relations and self.notes_relations == self:
             raise ValidationError("Заметка не может ссылаться сама на себя.")
                 
     def _add_reaction(self, profile, reaction_value):
+        # Проверяем существует ли уже реакция для данного профайла.
+        # Если нет, то создаём новую и корректирую карму создателя статьи.
+        # Если да, то меняем карму на противоположную
+        global COEFICIENT
         try:
             reaction = NotesReaction.objects.get(note=self, profile=profile)
+            if reaction.reaction != reaction_value:
+                if reaction_value:
+                    self.created_by.likes += COEFICIENT[self.category]
+                    self.created_by.dislikes -= 1
+                else:
+                    self.created_by.likes -= COEFICIENT[self.category]
+                    self.created_by.dislikes += 1
+           
             reaction.reaction = reaction_value
             reaction.save()
 
@@ -63,12 +107,20 @@ class Notes(BasicModelTemplate):
                 profile=profile,
                 reaction=reaction_value
             )
+            if reaction_value:
+                self.created_by.likes += COEFICIENT[self.category]
+            else:
+                self.created_by.dislikes += 1
+
 
     def like(self, profile):
         self._add_reaction(profile, 1)
+        self.likes += 1
 
     def dislike(self, profile):
-        self._add_reaction(profile, -1)
+        self._add_reaction(profile, 0)
+        self.dislikes += 1
+        self.created_by.dislikes += 1
             
     class Meta:
         verbose_name = ('note')
@@ -81,7 +133,7 @@ class NotesReaction(models.Model):
     id = models.UUIDField(default=uuid.uuid4, editable=True, primary_key=True)
     note = models.ForeignKey(Notes, on_delete=models.DO_NOTHING, related_name="notereactions_to_note")
     profile = models.ForeignKey(Profiles, on_delete=models.DO_NOTHING, related_name="notereactions_to_profile")
-    reaction = models.IntegerField()
+    reaction = models.BooleanField(default=True)
 
     class Meta:
         verbose_name = ('note reaction')
@@ -90,6 +142,10 @@ class NotesReaction(models.Model):
         constraints = [
                 models.UniqueConstraint(fields=['note', 'profile'], name="unique_note_profile")
             ]
+        
+    def __str__(self) :
+        reaction = "Like" if self.reaction else "Dislike"
+        return f"{self.note.title} - {self.profile.user.username} : {reaction}"
 
 
 '''
